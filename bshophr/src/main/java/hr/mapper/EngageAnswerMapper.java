149f2465018d16a04e284c92a837bbb88c821975
JaLyJoker <1041551225@qq.com>
2020-03-10 23:59:54 +0800

    搞了一个mapper,兄弟萌早点睡

---
+++ bshophr/src/main/java/hr/mapper/EngageAnswerMapper.java
@@ -0,0 +1,24 @@
+package hr.mapper;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import hr.pojo.EngageAnswer;
+
+@Repository
+public interface EngageAnswerMapper {
+	
+	public boolean insertEngageAnswer(EngageAnswer ea);
+	
+	public boolean deleteEngageAnswerById(int id);
+	
+	public boolean updateEngageAnswer(EngageAnswer ea);
+	
+	public EngageAnswer selectEngageAnswerById(int id);
+	
+	public List<EngageAnswer> selectEngageAnswerAll();
+
+	
+
+}
