2e40598f61c076d2256024593872e68ed151052b
Administrator <Administrator@PC-PJ0801>
2020-03-11 21:53:47 +0800

    改了数据库连接配置

--- bshophr/src/main/java/hr/web/controller/xsy/Salary_standard.java
+++ bshophr/src/main/java/hr/web/controller/xsy/Salary_standard.java
@@ -1,13 +1,23 @@
 package hr.web.controller.xsy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import hr.pojo.SalaryStandard;
+import hr.service.SalaryStandardService;
+
 @Controller
+@RequestMapping("/hr")
 public class Salary_standard {
-	@RequestMapping("/aaa.do")
-	public String aaa() {
-		System.out.println(000);
+	@Autowired
+	private SalaryStandardService service = null;
+
+	@RequestMapping("/salarystandard.do")
+	public String saveSalaryStandard(@ModelAttribute SalaryStandard ss) {
+		service.saveSalaryStandard(ss);
+		System.out.println("xsy345");
 		return "";
 	}
 
