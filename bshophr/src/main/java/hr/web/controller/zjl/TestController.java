2a231da0866aa9160511c35dc01911f435873d13
JaLyJoker <1041551225@qq.com>
2020-03-11 17:23:23 +0800

    111

--- bshophr/src/main/java/hr/web/controller/zjl/TestController.java
+++ bshophr/src/main/java/hr/web/controller/zjl/TestController.java
@@ -1,18 +1,27 @@
 package hr.web.controller.zjl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import hr.pojo.ConfigMajorKind;
+import hr.service.ConfigMajorKindService;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
 	
+	@Autowired
+	private ConfigMajorKindService cmks = null;
 	
 
 	@RequestMapping("/query.do")
 	public String tes(){
-		
+		List<ConfigMajorKind> s = cmks.findConfigMajorKindAll();
 		System.out.println("123");
+		System.out.println(s.size());
 		return null;
 	}
 	
