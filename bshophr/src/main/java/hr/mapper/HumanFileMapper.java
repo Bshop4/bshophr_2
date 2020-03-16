c8f34b3bed081d4358f9f1646773e316e64833b1
Administrator <Administrator@PC-PJ0801>
2020-03-16 11:49:46 +0800

    .

--- bshophr/src/main/java/hr/mapper/HumanFileMapper.java
+++ bshophr/src/main/java/hr/mapper/HumanFileMapper.java
@@ -5,12 +5,20 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import hr.pojo.HumanFile;
+
 @Repository
 public interface HumanFileMapper {
 	public List<HumanFile> selectHumanFileAll();
+
 	public HumanFile selectHumanFileById(int id);
+
+	// 向思宇加的用salary_standard_id查名字
+	public HumanFile selectHumanNameFileById(int id);
+
 	public boolean deleteHumanFileById(int id);
+
 	public boolean insertHumanFile(HumanFile hf);
+
 	public boolean updateHumanFile(HumanFile hf);
-	
+
 }
--- bshophr/src/main/java/hr/service/impl/HumanFileServiceImpl.java
+++ bshophr/src/main/java/hr/service/impl/HumanFileServiceImpl.java
@@ -10,45 +10,51 @@ import hr.pojo.HumanFile;
 import hr.service.HumanFileService;
 
 @Service
-public class HumanFileServiceImpl implements HumanFileService{
-	
+public class HumanFileServiceImpl implements HumanFileService {
+
 	@Autowired
-	private HumanFileMapper hfm=null;
-	
+	private HumanFileMapper hfm = null;
+
 	@Override
 	public List<HumanFile> findHumanFileAll() {
 		// TODO Auto-generated method stub
-		List<HumanFile> list=hfm.selectHumanFileAll();
+		List<HumanFile> list = hfm.selectHumanFileAll();
 		return list;
 	}
 
 	@Override
 	public HumanFile findHumanFileById(int id) {
 		// TODO Auto-generated method stub
-		HumanFile hf=hfm.selectHumanFileById(id);
+		HumanFile hf = hfm.selectHumanFileById(id);
 		return hf;
 	}
 
 	@Override
 	public boolean removeHumanFileById(int id) {
 		// TODO Auto-generated method stub
-		boolean flag=hfm.deleteHumanFileById(id);
-		
+		boolean flag = hfm.deleteHumanFileById(id);
+
 		return flag;
 	}
 
 	@Override
 	public boolean saveHumanFile(HumanFile hf) {
 		// TODO Auto-generated method stub
-		boolean flag=hfm.insertHumanFile(hf);
+		boolean flag = hfm.insertHumanFile(hf);
 		return flag;
 	}
 
 	@Override
 	public boolean updateHumanFile(HumanFile hf) {
 		// TODO Auto-generated method stub
-		boolean flag=hfm.updateHumanFile(hf);
+		boolean flag = hfm.updateHumanFile(hf);
 		return flag;
+	}
+
+	@Override
+	public HumanFile findHumanNameFileById(int id) {
+		HumanFile hf = hfm.selectHumanNameFileById(id);
+		return hf;
 	}
 
 }
